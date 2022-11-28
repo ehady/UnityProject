e66d37f74375126a84e66abfd323559f86f63609
Doğa Kurtoğlu <dogakurtoglu@hotmail.com>
2022-11-28 18:01:23 +0100

    Changed the ChildActif script
    I found out how to make objects visible/invisilbe, but I couldn't figure out how to trigger them when a collision happens. So in this version when you press B you can see the object that is invisible, we need to add an if statement to check for the collision

--- Unity_Project/Assets/scripts/chıldactıf.cs
+++ Unity_Project/Assets/scripts/chıldactıf.cs
@@ -1,15 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+
 
 public class chıldactıf : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    public Renderer ren;
+    private void Start()
     {
+        //object is invisible in the start
+        ren = GetComponent<Renderer>();
+        ren.enabled = false;
+    }
+    void  Update()
+    {
+        // Check if the button is pressed
+        bool check = Input.GetKey(KeyCode.B);
+
+        // Enable renderer accordingly
+        //this makes it visible
+        ren.enabled = check;
+    }
+
+    /*private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+
         if (collision.gameObject.CompareTag("box"))
         {
             gameObject.SetActive(true);
 
         }
-    }
-}
+    }*/ //for some reason this doesn't work, so I used the code above to test the renderer worked and it works. we just need to add a more correct if statement i think
+} 
