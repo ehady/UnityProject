9219bf96c9e2651dcb57ce2a880564ac583c7d7a
Doğa Kurtoğlu <dogakurtoglu@hotmail.com>
2022-12-05 15:26:31 +0100

    Level 1 changes
    Fixed the childactive script
    
    Changed the lever script but it doesn't work

--- Unity_Project/Assets/scripts/Lever.cs
+++ Unity_Project/Assets/scripts/Lever.cs
@@ -5,24 +5,49 @@ using UnityEngine;
 public class Lever : MonoBehaviour
 {
     Animator myAnimator;
-    private bool hasPlayer;
+    private bool keydown;   
     const string Moving = "LeverMove";
+    private bool hasPlayer;
     // Start is called before the first frame update
     void Start()
     {
         myAnimator= GetComponent<Animator>();
+        
     }
     
     private void Update()
     {
         //All the levers in the level is moving with this script
         //I tried using EnterCollision and ExitCollision but it didn'T work
-        if(Input.GetKeyDown(KeyCode.E))
+        
+        
+            if(hasPlayer && Input.GetKeyDown(KeyCode.E))
         {
             myAnimator.SetTrigger(Moving);
 
         }
+
+
+
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            hasPlayer= true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            hasPlayer= false;
+        }
+
+
+    }
+
 
 
 
--- Unity_Project/Assets/scripts/chıldactıf.cs
+++ Unity_Project/Assets/scripts/chıldactıf.cs
@@ -9,6 +9,7 @@ using static UnityEngine.RuleTile.TilingRuleOutput;
 public class chıldactıf : MonoBehaviour
 {
     public Renderer ren;
+    
     private void Start()
     {
         //object is invisible in the start
@@ -18,21 +19,21 @@ public class chıldactıf : MonoBehaviour
     void  Update()
     {
         // Check if the button is pressed
-        bool check = Input.GetKey(KeyCode.B);
+       // bool check = Input.GetKey(KeyCode.B);
 
         // Enable renderer accordingly
         //this makes it visible
-        ren.enabled = check;
+       // ren.enabled = check;
     }
 
-    /*private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         
 
         if (collision.gameObject.CompareTag("box"))
         {
-            gameObject.SetActive(true);
+            ren.enabled = true;
 
-        }
-    }*/ //for some reason this doesn't work, so I used the code above to test the renderer worked and it works. we just need to add a more correct if statement i think
-} 
+       }
+    }//for some reason this doesn't work, so I used the code above to test the renderer worked and it works. we just need to add a more correct if statement i think
+}
