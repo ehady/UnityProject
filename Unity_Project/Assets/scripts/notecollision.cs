dd53cc44b0e7610da5662cb45b2c27496c2972b4
Julia Vister <juliavister@gmail.com>
2022-12-05 15:37:03 +0100

    Level 2 changes
    added torches count text and stuff

--- Unity_Project/Assets/scripts/notecollision.cs
+++ Unity_Project/Assets/scripts/notecollision.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class notecollision : MonoBehaviour
 {
   private int note = 0;
+  [SerializeField] private Text keytext;
+  public AudioClip NoteSound;
+
   private void OnTriggerEnter2D(Collider2D collision)
   {
       if(collision.gameObject.CompareTag("Note"))
       {
           note++;
+          keytext.text = "Note : " + note;
+           Destroy(collision.gameObject);
       }
+      
   }
 }
